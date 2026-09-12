d4cd0921a78e1b9964881ee95afdfdf1b06c4b6e
MathRogelin <matheusrogelin15@gmail.com>
2026-09-12 10:07:03 -0300

    atualizando mensagem

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,8 @@ console.log("verificando...")
 function verificarJanela () {
     if (window.innerWidth <= 768) {
         console.log("Não é responsivo")
-        alert("Este site ainda não é responsivo. Recomendamos acessar pelo computador.");
+        alert("Este site ainda não é responsivo e está em manutenção. Recomendamos acessar pelo computador para melhor experiência.")
+        window.style.display = "none"
     } else{
         console.log("Responsivo...")
     }
